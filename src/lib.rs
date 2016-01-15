bab93887659256f8a4efe2f630bc93373ec92f56
dexter <dexter@dexterlabs.nl>
2016-01-15 01:36:09 +0100

    Fixed trait?

--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,10 @@ impl BeforeMiddleware for PostgresMiddleware {
         req.extensions.insert::<PostgresMiddleware>(Value(self.pool.clone()));
         Ok(())
     }
+
+    fn catch(&self, _: &mut Request, err: IronError) -> IronResult<()> {
+        Err(err)
+    }
 }
 
 /// Adds a method to requests to get a database connection.
